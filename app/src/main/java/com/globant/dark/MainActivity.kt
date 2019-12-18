0e518bc155e30a0c02293effefa0455e145279bf
Jose Quiroz <jv.quiroz@gmail.com>
2019-12-18 17:20:26 -0500

    Dark Mode with Peptasia Fonts

--- app/src/main/java/com/globant/dark/MainActivity.kt
+++ app/src/main/java/com/globant/dark/MainActivity.kt
@@ -2,11 +2,17 @@ package com.globant.dark
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().add(R.id.fragment_layout, WelcomeFragment()).commit()
+        }
+
     }
 }
--- app/src/main/java/com/globant/dark/WelcomeFragment.kt
+++ app/src/main/java/com/globant/dark/WelcomeFragment.kt
@@ -16,18 +16,20 @@
 
 package com.globant.dark
 
-import android.app.Application
-import androidx.preference.PreferenceManager
-import com.example.android.darktheme.ThemeHelper
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 
-class DarkThemeApplication : Application() {
 
-    override fun onCreate() {
-        super.onCreate()
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
-        val themePref = sharedPreferences.getString("themePref", ThemeHelper.DEFAULT_MODE)
-        if (themePref != null) {
-            ThemeHelper.applyTheme(themePref)
-        }
+class WelcomeFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    companion object {
+        internal val TAG = "VectorDrawableFragmentTag"
     }
 }
